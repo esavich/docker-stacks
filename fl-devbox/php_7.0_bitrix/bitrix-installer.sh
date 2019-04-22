3f229d881b7dcbfa5ae2c59524de356ff821c8ec
Evgeniy Savich <evgeniy@directline.pro>
2019-04-22 16:32:37 +0700

    remove fl-devbox, change repository from foritel to directlinepro in compose files

--- fl-devbox/php_7.0_bitrix/bitrix-installer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-BITRIX_URL=`echo "https://www.1c-bitrix.ru/download/${BITRIX_REDACTION:-start}_encode_php5.tar.gz" | tr '[:upper:]' '[:lower:]'`
-wget -qO- $BITRIX_URL | tar -xz -C .
-chown -R www-data:www-data ./*
-rm $0
-apache2-foreground > /dev/null 2>&1
